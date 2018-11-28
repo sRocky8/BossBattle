5565b2017339aec0b33fb684d4809755be598b5d
sRocky8 <sroccapr@student.fitchburgstate.edu>
2018-11-28 03:13:50 -0500

    Basic player control
    Added basic player control, Model and Idle animation in. Need Textures.

---
+++ Boss Battle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	//Public Variables
+	public float walkSpeed;
+	public float rotationSpeed;
+
+	//Private Variables
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		transform.Translate (0.0f, 0.0f, moveVertical * walkSpeed);
+		transform.Rotate(0.0f, moveHorizontal * rotationSpeed, 0.0f);
+	}
+}
